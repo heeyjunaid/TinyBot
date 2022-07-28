ff0ffa870ac58ffde155d8d201a229717711f0c9
Junaid Shaikh <junaidlatur@gmail.com>
2022-07-28 19:12:23 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='tinybot',
+      version='0.0.1',
+      description='Minimal virtual agent creation library',
+      author='Junaid Shaikh',
+      author_email='junaidlatur@gmail.com',
+      url='https://github.com/heeyjunaid/TinyBot',
+      packages=['tinybot'],
+     )
