0f80c391946c2d398cc3f0d2ce9308a33e1d3ee2
Junaid Shaikh <junaidlatur@gmail.com>
2022-07-28 19:21:34 +0530

    minor changes

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup
 
+
 setup(name='tinybot',
       version='0.0.1',
       description='Minimal virtual agent creation library',
@@ -7,4 +8,11 @@ setup(name='tinybot',
       author_email='junaidlatur@gmail.com',
       url='https://github.com/heeyjunaid/TinyBot',
       packages=['tinybot'],
+      install_requires=[
+            "PyYAML",
+            "scikit-learn==1.1.1",
+            "torch==1.12.0",
+            "transformers==4.20.1",
+            "numpy==1.23.1"
+      ]
      )
