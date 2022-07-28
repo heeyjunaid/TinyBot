4335556c4d57381c7220aa590327d59eb3be425b
Junaid Shaikh <junaidlatur@gmail.com>
2022-07-28 19:28:23 +0530

    minor changes

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(name='tinybot',
       packages=['tinybot'],
       install_requires=[
             "PyYAML",
-            "scikit-learn==1.1.1",
-            "torch==1.12.0",
-            "transformers==4.20.1",
-            "numpy==1.23.1"
+            "scikit-learn>=1.0.0",
+            "torch>=1.12.0",
+            "transformers>=4.20.1",
+            "numpy>1.20.0"
       ]
      )
